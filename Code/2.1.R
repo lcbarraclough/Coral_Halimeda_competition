d991ede5c3bd5e8fc3a68736a928a08b13b40fbc
lcbarraclough <s1729795@ed.ac.uk>
2023-05-10 11:07:38 +0100

    Laura made changes to graphs.

--- Code/2.1.R
+++ Code/2.1.R
@@ -26,4 +26,27 @@ summary(coral_m2)
 #ggsave(m2_plot, file = "Outputs/m2_plot.png", width = 10, height = 5)
 
 # Plot 2 ----
+coral_a <- coral[-c(30),]
+
+#   try running model again with out large outlier
+coral_m2.2 <-lmer(Total_hard_CC. ~ Halimeda_cover. +
+                    (1|Depth_m/ Site_Name),
+                  data = coral_a)
+plot(coral_m2.2)
+print(summary(coral_m2.2))
+
+#Plot this
+(m2.2_plot <- ggplot(coral_a, aes(x = Halimeda_cover.,
+                              y = Total_hard_CC.,
+                              colour = Depth_m)) +
+    facet_wrap(~Site_Name, nrow = 1) +
+    theme_classic() +
+    xlab("Halimeda cover (%)") +
+    ylab("Total hard coral cover (%)") +
+  geom_point(alpha = 1) +
+    geom_line(data = cbind(coral_a, pred = predict(coral_m2.2)),
+              aes(y = pred),
+              size = 1))
+
+#ggsave(m2.2_plot, file = "Outputs/m2.2_plot.png", width = 10, height = 5)
 
